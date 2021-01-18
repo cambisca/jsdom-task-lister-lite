4741c42f7abe14abf96101f990e5359dfc15222e
Cameron Bisca <cjbisca@gmail.com>
2021-01-17 23:04:37 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let newTaskForm = document.querySelector("form#create-task-form")
+let taskUl = document.querySelector("ul#tasks")
+
+newTaskForm.addEventListener("submit", function(event){
+  event.preventDefault()
+  let userInput = event.target["new-task-description"]
+  let whatTheUserTyped = userInput.value 
+
+  let blankLi = document.createElement("li")
+  blankLi.innerText = whatTheUserTyped
+  taskUl.append(blankLi)
+
+  blankLi.addEventListener("contextmenu", function(e){
+    e.preventDefault()
+    blankLi.remove()
+  })
+  
+})
+
+
